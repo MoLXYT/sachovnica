a2560b8e4b8381d7e28d89d756821603ca2b293f
GabooS <gabriel.csokas@gmail.com>
2026-09-22 07:48:50 +0200

    Generovanie 64 poli cyklom v TypeScripte

--- dist/main.js
+++ dist/main.js
@@ -1,15 +1,12 @@
+"use strict";
 const BOARD_SIZE = 8;
-
 const board = document.getElementById("board");
-
 if (board) {
     for (let row = 0; row < BOARD_SIZE; row++) {
         for (let col = 0; col < BOARD_SIZE; col++) {
             const square = document.createElement("div");
-
             square.classList.add("square");
             square.classList.add((row + col) % 2 === 0 ? "light" : "dark");
-
             board.appendChild(square);
         }
     }
